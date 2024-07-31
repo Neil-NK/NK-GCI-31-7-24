c74cef926c5263ed879420a4409d243f0d3dc4d7
Neil <34003574+Neil-NK@users.noreply.github.com>
2024-07-31 16:53:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3002;
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
